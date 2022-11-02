f986cfb594dbb07dc432bb0d87a9434099e327dc
Alexis Fonte <alexis.e.fonte@gmail.com>
2022-11-01 19:59:19 -0500

    commit tree ef26a1087de9daeab95884bcd987b7c126ff7796

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "Brian",
+    streetAddress: "11 Broadway"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+//employee[key] = value;
+    const employees =  { ...employee };
+    employees.name = "Sam";
+    employees.streetAddress = "11 Broadway";
+    return employees;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//employee[key] = value;
+
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key){
+   const deleteEmployee = { ...employee};
+    delete deleteEmployee.name;
+    return deleteEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee;
+};
